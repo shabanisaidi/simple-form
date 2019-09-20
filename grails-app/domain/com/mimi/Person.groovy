c81bcc9b396566415df6c7f58b5ca232ff8e29fa
shabanisaidi <saumtoto@gmail.com>
2019-09-20 14:55:14 +0300

    initial commit

---
+++ grails-app/domain/com/mimi/Person.groovy
@@ -0,0 +1,11 @@
+package com.mimi
+
+class Person {
+	String email
+	String address
+	String name
+	
+    static constraints = {
+    	email email: true
+    }
+}
---
+++ grails-app/init/tunu/BootStrap.groovy
@@ -0,0 +1,9 @@
+package tunu
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
